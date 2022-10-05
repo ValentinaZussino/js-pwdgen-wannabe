eae3809e18522cd3846fbd871f47b8fde2c063aa
ValentinaZussino <vzussino@protonmail.com>
2022-10-05 15:26:43 +0200

    prova mantenim risultato psw minuscolo

--- js/script.js
+++ js/script.js
@@ -5,8 +5,11 @@
 // Infine scrivi sulla pagina nomecognomecolorepreferito21 (dove il 21 è in questo caso l'età)
 
 const nomeUtente = prompt('Scrivi il tuo nome');
+const nomeUtLower = nomeUtente.toLowerCase();
 const cognomeUtente = prompt('Scrivi il tuo cognome');
+const cognomeUtLower = cognomeUtente.toLowerCase();
 const coloreUtente = prompt('Scrivi il tuo colore preferito');
+const coloreUtLower = coloreUtente.toLowerCase();
 const etaUtente = prompt('Scrivi la tua età in numero');
 const yourNewPsw = document.getElementById('psw-generated');
-yourNewPsw.innerHTML = nomeUtente+cognomeUtente+coloreUtente+etaUtente;
+yourNewPsw.innerHTML = nomeUtLower+cognomeUtLower+coloreUtLower+etaUtente;
